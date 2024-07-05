25970d61c0ddb5d6ede766d2c7f70af048f9e4ec
James Ives <iam@jamesiv.es>
2024-07-05 15:56:41 +0000

    Deploy Production Code for Commit a6f05dbaeaa50bd399becd720f9768cc2ad8dbda 🚀

--- lib/lib.d.ts
+++ lib/lib.d.ts
@@ -1,8 +1,8 @@
-import { ActionInterface } from './constants';
+import { ActionInterface, Status } from './constants';
 import { generateFile, getSponsors } from './template';
 /** Initializes and runs the action.
  *
  * @param {ActionInterface} configuration - The configuration object.
  */
-export default function run(configuration: ActionInterface): Promise<void>;
+export default function run(configuration: ActionInterface): Promise<Status>;
 export { generateFile, getSponsors, ActionInterface };
