8150fcb1bd2537ac414c1ef14a772f15a5716e42
James Ives <iam@jamesiv.es>
2024-09-01 09:04:07 -0400

    Revert "Merge branch 'releases/v1' into dev"
    This reverts commit 91918fb1b114799a5c5f201e47c75e25506fdc37, reversing
    changes made to abb3efba355f5006d8826dd4673c4d3c57b805e5.

--- lib/lib.d.ts
+++
@@ -1,8 +0,0 @@
-import { ActionInterface, Status } from './constants';
-import { generateFile, getSponsors } from './template';
-/** Initializes and runs the action.
- *
- * @param {ActionInterface} configuration - The configuration object.
- */
-export default function run(configuration: ActionInterface): Promise<Status>;
-export { generateFile, getSponsors, ActionInterface };
